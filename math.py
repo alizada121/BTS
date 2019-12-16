3a20b6fdc30ef8c06bfa3eac0cbfc66350ed3de7
Shabnam <58487057+Shabnamnam@users.noreply.github.com>
2019-12-16 10:32:46 +0400

    math.py

--- math.py
+++ math.py
@@ -1,8 +1,10 @@
 
-hi=int(input("write number")
+hi=int(input("type number:")
 if hi==0:
  print "Number equals to zero"
 elif hi>0:
  print "positive num"
 elif hi<0:
  print "negative"
+else:
+ print "you didn't type number"
